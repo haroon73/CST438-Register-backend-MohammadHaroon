f66bc7612079942cbcdbbebe449916a55db73eb9
Mohammad Haroon <mharoon@csumb.edu>
2023-10-10 21:07:40 -0700

    check

--- src/main/java/com/cst438/service/GradebookServiceMQ.java
+++ src/main/java/com/cst438/service/GradebookServiceMQ.java
@@ -27,32 +27,49 @@ public class GradebookServiceMQ implements GradebookService {
 	EnrollmentRepository enrollmentRepository;
 
 	Queue gradebookQueue = new Queue("gradebook-queue", true);
-
 	@Bean
 	public Queue createQueue() {
 		return new Queue("registration_queue", true);
 	}
-
 	// send message to grade book service about new student enrollment in course
 	@Override
 	public void enrollStudent(String student_email, String student_name, int course_id) {
 		System.out.println("Start Message "+ student_email +" " + course_id);
 		// create EnrollmentDTO, convert to JSON string and send to gradebookQueue
 		// TODO
+		// Create an EnrollmentDTO with the provided data
+		EnrollmentDTO enrollmentDTO = new EnrollmentDTO(0, student_email, student_name, course_id);
+
+		// Convert the EnrollmentDTO to a JSON string using your JSON utility class
+		String jsonEnrollment = asJsonString(enrollmentDTO);
+
+		// Use RabbitTemplate to send the JSON message to the gradebook-queue
+		rabbitTemplate.convertAndSend(gradebookQueue.getName(), jsonEnrollment);
 	}
 
 	@RabbitListener(queues = "registration-queue")
 	@Transactional
 	public void receive(String message) {
 		System.out.println("Receive grades :" + message);
-		/*
-		 * for each student grade in courseDTOG,  find the student enrollment
-		 * entity and update the grade.
-		 */
 
-		// deserialize the string message to FinalGradeDTO[]
+		FinalGradeDTO[] finalGrades = fromJsonString(message, FinalGradeDTO[].class);
 
-		// TODO
+		for (FinalGradeDTO grade : finalGrades) {
+			// Find the student enrollment entity based on student email and course ID
+			Enrollment enrollment = enrollmentRepository.findByEmailAndCourseId(grade.studentEmail(), grade.courseId());
+
+			if (enrollment != null) {
+				// Update the enrollment record with the final grade
+				enrollment.setCourseGrade(grade.grade());
+
+				// Save the updated enrollment record to the database
+				enrollmentRepository.save(enrollment);
+			} else {
+				// Handle the case where the enrollment record was not found
+				System.out.println("Enrollment record not found for student " + grade.studentEmail() +
+						" and course " + grade.courseId());
+			}
+		}
 
 	}
 
